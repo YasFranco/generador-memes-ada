e04414523e04aae03b30f926bae9e5db7be87bf5
yasminfranco <yasfranco98@gmail.com>
2024-12-26 10:51:11 -0300

    Corrigiendo evento de url

--- main.js
+++ main.js
@@ -31,13 +31,15 @@ $asideTxt.style.display = 'none';
 
 // barra imagen
 $inputUrlImg.addEventListener("input", () => {
-    $imgMeme.src = `${$inputUrlImg.value}`
+    $imgMeme.style.backgroundImage = `url(${$inputUrlImg.value})`
 })
 
 // ver evento de fondo 
 $inputImgBg.addEventListener("input", () =>{
-    $imgMeme.backgroundColor = `${$inputImgBg.value}` 
+    $imgMeme.style.backgroundColor = `${$inputImgBg.value}` 
 })
+
+
 
 
 
